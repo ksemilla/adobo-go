6963b8e65f08e57396244025e38d8d89e1ebbc16
Kevin Semilla <kevinsemilla@gmail.com>
2023-06-08 10:32:46 +1000

    Add go chi for routing

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Println("test")
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+	http.ListenAndServe(":3000", r)
 }
